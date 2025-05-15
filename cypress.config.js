1b68f24d490e2881c6ea72010be8d6e3659c4c22
Aldwin J <aldwinj12345@gmail.com>
2025-05-15 15:48:00 +0800

    bothconfigandworkflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter', //for html report
-  reporterOptions: {
-    overwrite: false,
-    html: false,
-    json: true
-  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on); //html report
-
-      // You can add other node event listeners here if needed
-    },
-    retries: {
-      runMode: 2, // Configure retries for `cypress run`
-      openMode: 0, // Configure retries for `cypress open`
     },
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+})
