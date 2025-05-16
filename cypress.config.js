cdfe465788a3a39c96999c31581d0b573b6b7ba8
Aldwin J <aldwinj12345@gmail.com>
2025-05-16 16:17:01 +0800

    update cypress config.js for screenshot and video when fail

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+    screenshotOnRunFailure: true, //this would enable when test fail
+    video: true, //enable video recording
+    videoCompression: false, //optional: this means disbaling compression for better quality
+    videoUploadOnPasses: false, //only upload videos for failed tests
   },
   reporter: 'mochawesome',
   reporterOptions: {
