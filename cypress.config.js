e4191efa14e3b1b1a7d6612607186e22a4c6ae54
Aldwin J <aldwinj12345@gmail.com>
2025-05-16 23:54:55 +0800

    adjust

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/mochawesome-report', // Ensure this is the desired output directory
+    reportDir: 'cypress/mochawesome-report',
     overwrite: false,
-    html: false,
-    json: true,
+    html: true, // Set this to true to ensure HTML report is generated
+    json: true, // Keep this true to generate the JSON report
+    jsonFilename: 'mochawesome', // Optional: set a specific filename for the JSON report
+    timestamp: false, // Optional: remove timestamp from filename
   },
   e2e: {
     setupNodeEvents(on, config) {
